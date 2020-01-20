31545910782fd668fd08c019dc5ef88affa4c405
Byron Marohn <combustible@live.com>
2020-01-20 11:53:16 -0800

    Initial commit
    Signed-off-by: Byron Marohn <combustible@live.com>

---
+++ README.md
@@ -0,0 +1,2 @@
+# Library of Souls
+An extension of NBTEditor to allow managing a vast library of Minecraft mobs
---
+++ plugin/src/main/java/com/playmonumenta/libraryofsouls/LibraryOfSouls.java
@@ -0,0 +1,15 @@
+package com.playmonumenta.libraryofsouls;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LibraryOfSouls extends JavaPlugin {
+	@Override
+	public void onEnable() {
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+}
