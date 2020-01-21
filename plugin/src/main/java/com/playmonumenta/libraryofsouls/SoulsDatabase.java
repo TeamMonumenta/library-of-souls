44fef450d8e8aa8da209a9fb58ef9db14c36bcd5
Byron Marohn <combustible@live.com>
2020-01-20 22:08:45 -0800

    Add placeholder items and working pages
    Signed-off-by: Byron Marohn <combustible@live.com>

--- plugin/src/main/java/com/playmonumenta/libraryofsouls/SoulsDatabase.java
+++ plugin/src/main/java/com/playmonumenta/libraryofsouls/SoulsDatabase.java
@@ -6,10 +6,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.Plugin;
 
 import com.goncalomb.bukkit.mylib.reflect.NBTTagCompound;
+import com.goncalomb.bukkit.mylib.reflect.NBTTagList;
 import com.goncalomb.bukkit.nbteditor.bos.BookOfSouls;
 import com.goncalomb.bukkit.nbteditor.nbt.EntityNBT;
 import com.google.gson.Gson;
@@ -18,7 +21,129 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.playmonumenta.libraryofsouls.utils.FileUtils;
 
+import net.md_5.bungee.api.ChatColor;
+
 public class SoulsDatabase {
+	public static class SoulSlot {
+		private final ItemStack mPlaceholder;
+		private final ItemStack mBoS;
+
+		private SoulSlot(NBTTagCompound nbt) {
+			EntityNBT entityNBT = EntityNBT.fromEntityData(nbt);
+
+			mBoS = (new BookOfSouls(entityNBT)).getBook();
+
+			switch (entityNBT.getEntityType()) {
+				case BLAZE:
+					mPlaceholder = new ItemStack(Material.BLAZE_POWDER);
+					break;
+				case CAVE_SPIDER:
+					mPlaceholder = new ItemStack(Material.FERMENTED_SPIDER_EYE);
+					break;
+				case CHICKEN:
+					mPlaceholder = new ItemStack(Material.CHICKEN);
+					break;
+				case COW:
+					mPlaceholder = new ItemStack(Material.BEEF);
+					break;
+				case CREEPER:
+					mPlaceholder = new ItemStack(Material.CREEPER_HEAD);
+					break;
+				case DROWNED:
+					mPlaceholder = new ItemStack(Material.TRIDENT);
+					break;
+				case ENDERMAN:
+					mPlaceholder = new ItemStack(Material.ENDER_PEARL);
+					break;
+				case GHAST:
+					mPlaceholder = new ItemStack(Material.GHAST_TEAR);
+					break;
+				case GUARDIAN:
+					mPlaceholder = new ItemStack(Material.PUFFERFISH);
+					break;
+				case ILLUSIONER:
+					mPlaceholder = new ItemStack(Material.BOW);
+					break;
+				case IRON_GOLEM:
+					mPlaceholder = new ItemStack(Material.IRON_BLOCK);
+					break;
+				case MAGMA_CUBE:
+					mPlaceholder = new ItemStack(Material.MAGMA_CREAM);
+					break;
+				case PHANTOM:
+					mPlaceholder = new ItemStack(Material.PHANTOM_MEMBRANE);
+					break;
+				case SHULKER:
+					mPlaceholder = new ItemStack(Material.SHULKER_BOX);
+					break;
+				case SKELETON:
+					mPlaceholder = new ItemStack(Material.SKELETON_SKULL);
+					break;
+				case SLIME:
+					mPlaceholder = new ItemStack(Material.SLIME_BALL);
+					break;
+				case SNOWMAN:
+					mPlaceholder = new ItemStack(Material.CARVED_PUMPKIN);
+					break;
+				case SPIDER:
+					mPlaceholder = new ItemStack(Material.SPIDER_EYE);
+					break;
+				case VINDICATOR:
+					mPlaceholder = new ItemStack(Material.STONE_AXE);
+					break;
+				case WITCH:
+					mPlaceholder = new ItemStack(Material.POISONOUS_POTATO);
+					break;
+				case WITHER:
+					mPlaceholder = new ItemStack(Material.WITHER_SKELETON_SKULL);
+					break;
+				case WOLF:
+					mPlaceholder = new ItemStack(Material.BONE);
+					break;
+				case ZOMBIE:
+					mPlaceholder = new ItemStack(Material.ZOMBIE_HEAD);
+					break;
+				default:
+					mPlaceholder = mBoS.clone();
+					break;
+			}
+
+			ItemMeta placeholderMeta = mPlaceholder.getItemMeta();
+			ItemMeta bosMeta = mBoS.getItemMeta();
+
+			List<String> lore = new ArrayList<String>();
+
+			/* TODO: Fix this custom name being JSON ugly */
+			String name = nbt.getString("CustomName");
+			if (name != null) {
+				placeholderMeta.setDisplayName(name);
+				bosMeta.setDisplayName(name);
+			}
+
+			if (nbt.hasKey("Health")) {
+				lore.add(ChatColor.WHITE + "Health: " + Double.toString(nbt.getDouble("Health")));
+			}
+
+			NBTTagList tags = nbt.getList("Tags");
+			if (tags != null && tags.size() > 0) {
+				lore.add(ChatColor.WHITE + "Tags: " + tags.toString());
+			}
+
+			placeholderMeta.setLore(lore);
+			bosMeta.setLore(lore);
+
+			mPlaceholder.setItemMeta(placeholderMeta);
+			mBoS.setItemMeta(bosMeta);
+		}
+
+		public ItemStack getPlaceholder() {
+			return mPlaceholder;
+		}
+
+		public ItemStack getBoS() {
+			return mBoS;
+		}
+	}
 	private static SoulsDatabase INSTANCE = null;
 
 	private List<NBTTagCompound> mSouls = new ArrayList<NBTTagCompound>();
@@ -29,11 +154,11 @@ public class SoulsDatabase {
 		INSTANCE = this;
 	}
 
-	public List<ItemStack> getSouls(int offset, int count) {
-		List<ItemStack> souls = new ArrayList<ItemStack>(count);
+	public List<SoulSlot> getSouls(int offset, int count) {
+		List<SoulSlot> souls = new ArrayList<SoulSlot>(count);
 
 		for (int i = offset; i < offset + count; i++) {
-			ItemStack bos = getSoul(i);
+			SoulSlot bos = getSoul(i);
 			if (bos != null) {
 				souls.add(bos);
 			}
@@ -42,18 +167,12 @@ public class SoulsDatabase {
 		return souls;
 	}
 
-	public ItemStack getSoul(int index) {
+	public SoulSlot getSoul(int index) {
 		if (index >= mSouls.size()) {
 			return null;
 		}
 
-		NBTTagCompound nbt = mSouls.get(index);
-
-		ItemStack book = (new BookOfSouls(EntityNBT.fromEntityData(nbt))).getBook();
-
-		/* TODO: Fancy up the book item a bit with more info - maybe a display item and a book item? */
-
-		return book;
+		return new SoulSlot(mSouls.get(index));
 	}
 
 	/* TODO: File watcher */
--- plugin/src/main/java/com/playmonumenta/libraryofsouls/SoulsInventory.java
+++ plugin/src/main/java/com/playmonumenta/libraryofsouls/SoulsInventory.java
@@ -1,45 +1,87 @@
 package com.playmonumenta.libraryofsouls;
 
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryCloseEvent;
-import org.bukkit.inventory.ItemStack;
 
 import com.goncalomb.bukkit.mylib.utils.CustomInventory;
+import com.goncalomb.bukkit.mylib.utils.UtilsMc;
+import com.playmonumenta.libraryofsouls.SoulsDatabase.SoulSlot;
 
 public class SoulsInventory extends CustomInventory {
+	private List<SoulSlot> mCurrentSlots;
+
 	private final SoulsDatabase mDB;
+	private int mOffset;
+	private boolean mHasPrevPage;
+	private boolean mHasNextPage;
 
 	public SoulsInventory(Player owner) {
 		super(owner, 54, "Library of Souls");
 
 		mDB = SoulsDatabase.getInstance();
+		mOffset = 0;
+		loadWindow();
+	}
 
-		for (ItemStack item : mDB.getSouls(0, 36)) {
-			_inventory.addItem(item);
+	private void loadWindow() {
+		mCurrentSlots = mDB.getSouls(mOffset, 36);
+
+		for (int i = 0; i < 36; i++) {
+			if (i < mCurrentSlots.size()) {
+				_inventory.setItem(i, mCurrentSlots.get(i).getPlaceholder());
+			} else {
+				_inventory.setItem(i, null);
+			}
+		}
+
+		if (mOffset > 0) {
+			_inventory.setItem(45, UtilsMc.newSingleItemStack(Material.ARROW, "[" + Integer.toString(mOffset / 36) + "] Previous Page"));
+			mHasPrevPage = true;
+		} else {
+			mHasPrevPage = false;
+			_inventory.setItem(45, null);
+		}
+
+		if (mCurrentSlots.size() >= 36) {
+			_inventory.setItem(53, UtilsMc.newSingleItemStack(Material.ARROW, "[" + Integer.toString(mOffset / 36) + "] Next Page"));
+			mHasNextPage = true;
+		} else {
+			mHasNextPage = false;
+			_inventory.setItem(53, null);
 		}
 	}
 
 	@Override
 	protected void inventoryClick(final InventoryClickEvent event) {
 		final int slot = event.getRawSlot();
-		if (slot >= 0 && slot < 54) {
-			final ItemStack item = event.getCurrentItem().clone();
+		if (slot >= 0 && slot < 36) {
 			if (event.isShiftClick()) {
+				event.setCurrentItem(mCurrentSlots.get(slot).getBoS());
 				Bukkit.getScheduler().runTask(getPlugin(), new Runnable() {
 					@Override
 					public void run() {
-						event.setCurrentItem(item);
+						event.setCurrentItem(mCurrentSlots.get(slot).getPlaceholder());
 					}
 				});
 			} else {
 				if (event.getCursor().getType() == Material.AIR) {
-					event.getView().setCursor(item);
+					event.getView().setCursor(mCurrentSlots.get(slot).getBoS());
 				}
 				event.setCancelled(true);
 			}
+		} else if (slot == 45 && mHasPrevPage) {
+			mOffset -= 36;
+			loadWindow();
+			event.setCancelled(true);
+		} else if (slot == 53 && mHasNextPage) {
+			mOffset += 36;
+			loadWindow();
+			event.setCancelled(true);
 		} else if (event.getCursor().getType() == Material.AIR) {
 			event.setCancelled(true);
 		}
