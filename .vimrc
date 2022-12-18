48e77f475bd98cd07a1c922deeb725cde26c1bd9
Byron Marohn <Combustible@live.com>
2022-12-18 02:33:22 +0000

    Convert project from maven to gradle build system
    Signed-off-by: Byron Marohn <Combustible@live.com>

--- .vimrc
+++ .vimrc
@@ -1,2 +1,13 @@
 " Remove whitespace from end of lines when saving
 autocmd BufWritePre * :%s/\s\+$//e
+
+" Use spaces in XML files
+autocmd Filetype java set noexpandtab
+
+" 4-width tabs by default
+set expandtab
+set shiftwidth=4
+set tabstop=4
+
+set errorformat=%E%f:%l:\ error:\ %m,%W[ant:checkstyle]\ [WARN]\ %f:%l:%m,%W%f:%l:%m,%+C%[\ %\\t]%.%#
+set makeprg=./gradlew\ clean\ build
---
+++ build.gradle.kts
@@ -0,0 +1,199 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.hidetake.groovy.ssh.core.Remote
+import org.hidetake.groovy.ssh.core.RunHandler
+import org.hidetake.groovy.ssh.core.Service
+import org.hidetake.groovy.ssh.session.SessionHandler
+import net.ltgt.gradle.errorprone.errorprone
+import net.ltgt.gradle.errorprone.CheckSeverity
+
+plugins {
+    java
+    `maven-publish`
+    id("com.palantir.git-version") version "0.12.2"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.1" // Generates plugin.yml
+    id("org.hidetake.ssh") version "2.10.1"
+    id("net.ltgt.errorprone") version "2.0.2"
+    id("net.ltgt.nullaway") version "1.3.0"
+    checkstyle
+    pmd
+}
+
+repositories {
+    mavenLocal()
+    maven {
+        url = uri("https://repo.papermc.io/repository/maven-public/")
+    }
+
+    maven {
+        url = uri("https://jitpack.io")
+    }
+
+    maven {
+        url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
+    }
+
+    maven {
+        url = uri("https://raw.githubusercontent.com/TeamMonumenta/monumenta-redis-sync/master/mvn-repo/")
+    }
+
+    // NBT API, pulled in by CommandAPI
+    maven {
+        url = uri("https://repo.codemc.org/repository/maven-public/")
+    }
+
+    maven {
+        url = uri("https://repo.maven.apache.org/maven2/")
+    }
+}
+
+dependencies {
+    compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
+    compileOnly("dev.jorel.CommandAPI:commandapi-core:6.0.0")
+    compileOnly("com.goncalomb.bukkit:nbteditor:3.3-SNAPSHOT")
+    compileOnly("com.playmonumenta:redissync:3.0")
+    compileOnly("com.google.code.gson:gson:2.8.5")
+    errorprone("com.google.errorprone:error_prone_core:2.10.0")
+    errorprone("com.uber.nullaway:nullaway:0.9.5")
+}
+
+group = "com.playmonumenta"
+val gitVersion: groovy.lang.Closure<String> by extra
+version = gitVersion()
+description = "libraryofsouls"
+java.sourceCompatibility = JavaVersion.VERSION_1_8
+java.targetCompatibility = JavaVersion.VERSION_1_8
+
+// Configure plugin.yml generation
+bukkit {
+    load = BukkitPluginDescription.PluginLoadOrder.POSTWORLD
+    main = "com.playmonumenta.libraryofsouls.LibraryOfSouls"
+    apiVersion = "1.18"
+    name = "LibraryOfSouls"
+    authors = listOf("The Monumenta Team")
+    depend = listOf("CommandAPI", "NBTEditor")
+    softDepend = listOf("MonumentaRedisSync")
+}
+
+pmd {
+    isConsoleOutput = true
+    toolVersion = "6.41.0"
+    ruleSets = listOf("$rootDir/pmd-ruleset.xml")
+    setIgnoreFailures(true)
+}
+
+publishing {
+    publications.create<MavenPublication>("maven") {
+        project.shadow.component(this)
+    }
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/TeamMonumenta/library-of-souls")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-Xmaxwarns")
+    options.compilerArgs.add("10000")
+    options.compilerArgs.add("-Xlint:deprecation")
+
+    options.errorprone {
+        option("NullAway:AnnotatedPackages", "com.playmonumenta")
+
+        allErrorsAsWarnings.set(true)
+
+        /*** Disabled checks ***/
+        // These we almost certainly don't want
+        check("InlineMeSuggester", CheckSeverity.OFF) // We won't keep deprecated stuff around long enough for this to matter
+        check("CatchAndPrintStackTrace", CheckSeverity.OFF) // This is the primary way a lot of exceptions are handled
+        check("FutureReturnValueIgnored", CheckSeverity.OFF) // This one is dumb and doesn't let you check return values with .whenComplete()
+        check("ImmutableEnumChecker", CheckSeverity.OFF) // Would like to turn this on but we'd have to annotate a bunch of base classes
+        check("LockNotBeforeTry", CheckSeverity.OFF) // Very few locks in our code, those that we have are simple and refactoring like this would be ugly
+        check("StaticAssignmentInConstructor", CheckSeverity.OFF) // We have tons of these on purpose
+        check("StringSplitter", CheckSeverity.OFF) // We have a lot of string splits too which are fine for this use
+        check("MutablePublicArray", CheckSeverity.OFF) // These are bad practice but annoying to refactor and low risk of actual bugs
+    }
+}
+
+val basicssh = remotes.create("basicssh") {
+    host = "admin-eu.playmonumenta.com"
+    port = 8822
+    user = "epic"
+    agent = System.getenv("IDENTITY_FILE") == null
+    identity = if (System.getenv("IDENTITY_FILE") == null) null else file(System.getenv("IDENTITY_FILE"))
+    knownHosts = allowAnyHosts
+}
+
+val adminssh = remotes.create("adminssh") {
+    host = "admin-eu.playmonumenta.com"
+    port = 9922
+    user = "epic"
+    agent = System.getenv("IDENTITY_FILE") == null
+    identity = if (System.getenv("IDENTITY_FILE") == null) null else file(System.getenv("IDENTITY_FILE"))
+    knownHosts = allowAnyHosts
+}
+
+tasks.create("stage-deploy") {
+    val shadowJar by tasks.named<ShadowJar>("shadowJar")
+    dependsOn(shadowJar)
+    doLast {
+        ssh.runSessions {
+            session(basicssh) {
+                put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/stage/m12/server_config/plugins")
+                execute("cd /home/epic/stage/m12/server_config/plugins && rm -f LibraryOfSouls.jar && ln -s " + shadowJar.archiveFileName.get() + " LibraryOfSouls.jar")
+            }
+        }
+    }
+}
+
+tasks.create("build-deploy") {
+    val shadowJar by tasks.named<ShadowJar>("shadowJar")
+    dependsOn(shadowJar)
+    doLast {
+        ssh.runSessions {
+            session(adminssh) {
+                put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/project_epic/server_config/plugins")
+                execute("cd /home/epic/project_epic/server_config/plugins && rm -f LibraryOfSouls.jar && ln -s " + shadowJar.archiveFileName.get() + " LibraryOfSouls.jar")
+                execute("cd /home/epic/project_epic/mobs/plugins && rm -f LibraryOfSouls.jar && ln -s ../../server_config/plugins/LibraryOfSouls.jar")
+            }
+        }
+    }
+}
+
+tasks.create("play-deploy") {
+    val shadowJar by tasks.named<ShadowJar>("shadowJar")
+    dependsOn(shadowJar)
+    doLast {
+        ssh.runSessions {
+            session(adminssh) {
+                put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m8/server_config/plugins")
+                put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m11/server_config/plugins")
+                put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m13/server_config/plugins")
+                put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m14/server_config/plugins")
+                put(shadowJar.archiveFile.get().getAsFile(), "/home/epic/play/m15/server_config/plugins")
+                execute("cd /home/epic/play/m8/server_config/plugins && rm -f LibraryOfSouls.jar && ln -s " + shadowJar.archiveFileName.get() + " LibraryOfSouls.jar")
+                execute("cd /home/epic/play/m11/server_config/plugins && rm -f LibraryOfSouls.jar && ln -s " + shadowJar.archiveFileName.get() + " LibraryOfSouls.jar")
+                execute("cd /home/epic/play/m13/server_config/plugins && rm -f LibraryOfSouls.jar && ln -s " + shadowJar.archiveFileName.get() + " LibraryOfSouls.jar")
+                execute("cd /home/epic/play/m14/server_config/plugins && rm -f LibraryOfSouls.jar && ln -s " + shadowJar.archiveFileName.get() + " LibraryOfSouls.jar")
+                execute("cd /home/epic/play/m15/server_config/plugins && rm -f LibraryOfSouls.jar && ln -s " + shadowJar.archiveFileName.get() + " LibraryOfSouls.jar")
+            }
+        }
+    }
+}
+
+fun Service.runSessions(action: RunHandler.() -> Unit) =
+    run(delegateClosureOf(action))
+
+fun RunHandler.session(vararg remotes: Remote, action: SessionHandler.() -> Unit) =
+    session(*remotes, delegateClosureOf(action))
+
+fun SessionHandler.put(from: Any, into: Any) =
+    put(hashMapOf("from" to from, "into" to into))
